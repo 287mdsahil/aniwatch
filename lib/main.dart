4d483f653dadcc9fd117e1437b1ec002504f601b
Md Sahil <287mdsahil@gmail.com>
2022-01-14 20:49:04 +0000

    Fetched Anime Titles from gogoanime

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart' as parser;
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -28,12 +29,35 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  String _searchTitle = "";
+  final GlobalKey<_SearchPageState> _formKey = GlobalKey<_SearchPageState>();
+
+  Widget _buildSearchTitleField() {
+    return Text("test");
+  }
+
+  Future getAnimesFromGogoanime() async {
+    Map<String, String> queryParams = {'keyword':'Attack on Titan'};
+    List<Anime> animeList = [];
+    print("Trying...");
+    var url = Uri.https("www3.gogoanime.cm", "search.html", queryParams);
+    print(url);
+    var response = await http.Client().get(url);
+    var document = parser.parse(response.body);
+    for(var a in document.getElementsByClassName("img")) {
+      Anime anime = Anime(a.children[0].attributes["title"] ?? "null");
+      animeList.add(anime);
+    }
+
+    print(document.getElementsByClassName("img")[0].children[0].attributes["title"]);
+    return animeList;
+  }
+
   Future getAnimes() async {
     Map<String, String> queryParams = {'q':'naruto'};
     var url = Uri.https("api.jikan.moe", "v3/search/anime", queryParams);
     var response = await http.get(url);
     var jsonData = jsonDecode(response.body);
-    print(jsonData);
     List<Anime> animeList = [];
 
     for(var a in jsonData['results']) {
@@ -51,22 +75,29 @@ class _SearchPageState extends State<SearchPage> {
         title: const Text('Search Page'),
       ),
       body: Container(
-        child:Card(
-          child: FutureBuilder(
-            future: getAnimes(),
-            builder: (context,AsyncSnapshot snapshot) {
-              if(snapshot.data == null) {
-                return Container(child:Center(child:Text("Loading")));
-              } else {
-                return ListView.builder(itemCount: snapshot.data.length, itemBuilder: (context, i){
-                  return ListTile(
-                    title: Text(snapshot.data[i].title),
-                  );
-                });
-              }
-            },
+        child: Column(
+          children: [
+          Form(child: Column(children:<Widget> [
+           _buildSearchTitleField(),
+           ElevatedButton(onPressed: () => {}, child: Text("Search"),)
+          ],)
           ),
-        )
+          Expanded(
+            child: FutureBuilder(
+              future: getAnimesFromGogoanime(),
+              builder: (context,AsyncSnapshot snapshot) {
+                if(snapshot.data == null) {
+                  return Container(child:Center(child:Text("Loading")));
+                } else {
+                  return ListView.builder(itemCount: snapshot.data.length, itemBuilder: (context, i){
+                    return ListTile(
+                        title: Text(snapshot.data[i].title),
+                        );
+                    });
+                }
+              }),
+            )
+          ])
       )
     );
   }
